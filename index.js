f6d0d13343292b55ce5c1c8d80bad0ac5f1995e9
Goldy Verma <749947@student.tor.loc>
2019-08-07 17:14:35 -0400

    final commit at 5:13pm

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended}));
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-    res.send('<h1>Hello world</h1>')
+    console.log(req.body);
+    res.send('GET sent')
 })
 app.post('/',function(req,res){
-    res.send('Hello world')
+ 
+    console.log(req.body);
+    res.send('POST sent')
+})
+app.put('/',function(req,res){
+    res.send('PUT sent')
+})
+app.delete('/',function(req,res){
+    res.send('DELETE sent')
 })
 app.listen(3000);
