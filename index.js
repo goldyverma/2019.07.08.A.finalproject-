05190afcf750c636aa32d49ac4d04e8cd9fa0282
Goldy Verma <749947@student.tor.loc>
2019-08-07 16:24:37 -0400

    fifth commit at 4:24pm

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const http = require('http');
-const data = {
-    firstName:'GOLDY',
-    lastName:'VERMA'
-}
+var express = require('express');
+var app = express();
 
-const site = http.createServer(function(req,res){
-    console.log("Hello World");
-    console.log(req.headers);
-    console.log(req.url);
-    res.setHeader('Content-Type','application/json');
-    //res.setHeader('Content-Type','text/html');
-    //res.write('<h1>Hello Class</h1>')
-    res.write(JSON.stringify(data));
-    res.end();
+app.get('/',function(req, res){
+    res.send("Hello World")
+});
 
-});  
-site.listen(3000);
+app.listen(3000);
