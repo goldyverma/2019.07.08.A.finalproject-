14b6020dba750dfb3139cbd19b51138a8607373c
Goldy Verma <749947@student.tor.loc>
2019-08-07 16:13:45 -0400

    fourth commit  at 4:13pm

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const http = require('http');
+const data = {
+    firstName:'GOLDY',
+    lastName:'VERMA'
+}
+
 const site = http.createServer(function(req,res){
     console.log("Hello World");
-});
+    console.log(req.headers);
+    console.log(req.url);
+    res.setHeader('Content-Type','application/json');
+    //res.setHeader('Content-Type','text/html');
+    //res.write('<h1>Hello Class</h1>')
+    res.write(JSON.stringify(data));
+    res.end();
+
+});  
 site.listen(3000);
