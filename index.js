ca3ba2d0b7ff71964f820264c7a387b56ec57797
Goldy Verma <749947@student.tor.loc>
2019-08-07 16:57:16 -0400

    seventh commit at 4:57pm

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req, res){
-    res.send("Hello World")
-});
-
+app.get('/',function(req,res){
+    res.send('<h1>Hello world</h1>')
+})
+app.post('/',function(req,res){
+    res.send('Hello world')
+})
 app.listen(3000);
